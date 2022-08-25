4fe25a87546ed462bfe0d9f2769aa18520a66555
S4LGado <juanessalgado2001@gmail.com>
2022-08-25 16:01:32 -0500

    ejercicio 1

---
+++ EJERCICIOS/1_TiposDeDatos.py
@@ -0,0 +1,32 @@
+# Crear variables y asignarles los siguientes tipos de datos
+
+# Enteros: 1, 2, 3, 999
+# Luego reste sucesivamente del último al primero y almacenelo
+# en una variable llamada resultado
+
+a = 1
+b = 2
+c = 3
+d = 999
+
+resultado = d-c-b-a
+
+print ("Resultado 1 : ", resultado)
+
+
+# Flotantes: 15.2, 29.5, 18.28
+# Luego divida sucesivamente del primero al último
+# y almacénelo en una variable llamada resultado2
+
+# Strings: "123", "Cristian"
+# Luego sume ambas variables y determina si la
+# opreación es posible, si es así almacénelo en una variable
+# de su elección
+
+####### Para pensar #######
+
+# Busque una manera de convertir:
+# un entero a un flotante
+# un flotante a un entero
+# un string a un entero y flotante
+# un número a un string
