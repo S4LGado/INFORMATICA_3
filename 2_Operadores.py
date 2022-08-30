d5429c40d495f166980a01e9a037018a86b83d3f
S4LGado <juanessalgado2001@gmail.com>
2022-08-30 14:51:29 -0500

    Actualizaciones

---
+++ 2_Operadores.py
@@ -0,0 +1,40 @@
+# Operador de asignación
+
+a = 1
+b = 2
+c = 5
+
+# Operadores ariméticos
+# Realice las siguientes operaciones mentalmente : 3 + 9
+                                    # 3.0 + 9
+                                    # 9 ** 0.5
+                                    # 2 ** 32
+                                    # 19 // 2
+                                    # 19 % 3 ***
+                                    # "hola" + "mundo"
+                                    # "hola" * 3
+                                    # ["A"] + [1,2,3]
+                                    # [] + []
+                                    # (1,2,3) + (1,)
+
+
+# Realice las sigueintes operaciones mentalente : True and True
+                                                # False and True
+                                                # False and False
+                                                # not True
+                                                # not False
+                                                # True or True
+                                                # False or True
+                                                # 1 and 1
+                                                # 0 and 1
+                                                # 1 and 3
+                                                # 1 and "hola"
+                                                # 0 and 3
+                                                # 0 and "hola"
+                                                # "hola" or "verdadero"
+                                                # 1 or 3
+                                                # "" or "hola"
+                                                # "" or "false"
+
+d = "" or False
+print(d)
