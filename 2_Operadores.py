36903ab8bb3c65ca3d5ea93b84c678dff64a46ec
S4LGado <juanessalgado2001@gmail.com>
2022-08-30 15:18:27 -0500

    a

--- 2_Operadores.py
+++ 2_Operadores.py
@@ -5,36 +5,60 @@ b = 2
 c = 5
 
 # Operadores ariméticos
-# Realice las siguientes operaciones mentalmente : 3 + 9
-                                    # 3.0 + 9
-                                    # 9 ** 0.5
-                                    # 2 ** 32
-                                    # 19 // 2
-                                    # 19 % 3 ***
-                                    # "hola" + "mundo"
-                                    # "hola" * 3
-                                    # ["A"] + [1,2,3]
-                                    # [] + []
-                                    # (1,2,3) + (1,)
+""" Realice las siguientes operaciones mentalmente : 
+3 + 9
+3.0 + 9
+9 ** 0.5
+2 ** 32
+19 // 2
+19 % 3 ***
+"hola" + "mundo"
+"hola" * 3
+["A"] + [1,2,3]
+[] + []
+(1,2,3) + (1,) """
 
+# Operadores Lógicos
+""" Realice las sigueintes operaciones mentalente : 
+True and True
+False and True
+False and False
+not True 
+not False
+True or True
+False or True
+1 and 1
+0 and 1
+1 and 3
+1 and "hola"
+0 and 3
+0 and "hola"
+"hola" or "verdadero"
+1 or 3"" or "hola"
+"" or "false" """
 
-# Realice las sigueintes operaciones mentalente : True and True
-                                                # False and True
-                                                # False and False
-                                                # not True
-                                                # not False
-                                                # True or True
-                                                # False or True
-                                                # 1 and 1
-                                                # 0 and 1
-                                                # 1 and 3
-                                                # 1 and "hola"
-                                                # 0 and 3
-                                                # 0 and "hola"
-                                                # "hola" or "verdadero"
-                                                # 1 or 3
-                                                # "" or "hola"
-                                                # "" or "false"
+# Operadores de comparación 
+""" Realizar las siguientes operaciones: 
+1 > 2
+1 < 3
+1 == 1
+2 != 1
+3 >= 3
+5 <= 2
+4 > True
+True > False
+[] > [1,2,3]
+"a" > "b" """
 
-d = "" or False
+# Operadores de pertenencia
+
+""" Realizar las siguientes operaciones:
+"a" in "abcdefg"
+"A" in "ABCDEFG"
+1 in [1, 2, 3]
+1 in ["1", "2", "3"]
+"hola" in "holamundo"
+"Hola" not in "holamundo" """
+
+d = "hola" in "holamundo"
 print(d)
