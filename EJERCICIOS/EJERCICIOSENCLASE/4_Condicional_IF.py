40b1540a9f68202076337533dd51a21336970ea3
S4LGado <69695137+S4LGado@users.noreply.github.com>
2022-09-06 15:32:30 -0500

    clase

--- EJERCICIOS/EJERCICIOSENCLASE/4_Condicional_IF.py
+++ EJERCICIOS/EJERCICIOSENCLASE/4_Condicional_IF.py
@@ -1,5 +1,6 @@
 #06-09-2022
 
+# Ejercicio 1
 """
 Pida a un usuario su nombre y su edad. Determine si es mayor de edad,
 y muestre un mensaje en pantalla diciendo:
@@ -14,20 +15,78 @@ if edad >= 18:
 elif 0 < edad < 18:
     print(nombre, "{}, usted es menor de edad", format(nombre))    
 """
-
+# Ejercicio 2
 """
 Realice un programa que calcule el mayor de tres numeros
 """
-
+"""
 a = float(input("Digite el numero 1: "))
 b = float(input("Digite el numero 2: "))
 c = float(input("Digite el numero 3: "))
 
-if a > b and a > c:
+if a >= b and a >= c:
     mayor = a
-elif b > a and b > c:
+elif b >= a and b >= c:
     mayor = b
-elif c > a and c > b:
+elif c >= a and c >= b:
     mayor = c
 
 print ("El mayor numero entre los ingresados es: ",mayor)
+
+"""
+# Ejercicio 3
+"""
+
+Salario base = 1000000
+
+Realice un programa que calcule el salario de un vendedor
+de seguros, teniendo en cuenta las siguientes condiciones =>
+
+=> ventas => entre [5, 20] seguros =>
+    aumento del 20% sobre la base
+=> ventas => entre [21, 50] seguros =>
+    aumento del 30% sobre la base
+=> ventas => entre [51, infinito] seguros =>
+    aumento del 35% sobre la base        
+
+"""
+"""
+salario = 1000000
+ventas=float(input("Digite la cantidad de ventas que realizc en este mes:"))
+
+if 20 >= ventas >= 5:
+    salario = 1000000*1.2
+elif 50 >= ventas >= 21:
+    salario = 1000000*1.3
+elif ventas >= 51:
+    salario = 1000000*1.35
+
+print ("Segun sus ventas el salario que devenga en este mes es de: ",salario)
+"""
+
+# Ejercicio 4
+
+"""
+Una contrasena de un programa, debe incluir:
+
+* Contrasena contenga mayusculas
+* Contrasena contenga minusculas
+* Contenga numeros
+* Caracteres especiales
+* Por lo menos 8 caracteres en total
+
+Determine si al ingresar una contrasena, se cumple con todas las condiciones.
+
+"""
+
+condicion1 = <Contenga mayusculas>
+condicion2 = <Contenga minusculas>
+condicion3 = <Contenga numeros>
+condicion4 = <Caracteres especiales>
+condicion5 = <Por lo menos 8 caracteres en total>
+
+if condicion1 and condicion2 and condicion3 and condicion4 and condicion5:
+    print ("Es valido").
+
+elif 
+    print ("No es valido")
