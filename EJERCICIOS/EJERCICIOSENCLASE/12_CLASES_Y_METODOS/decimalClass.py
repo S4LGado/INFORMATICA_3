6a3075d43763e92bdb827b99289c82d6e3bdc46d
S4LGado <juanessalgado2001@gmail.com>
2022-11-10 16:25:07 -0500

    10/11

---
+++ EJERCICIOS/EJERCICIOSENCLASE/12_CLASES_Y_METODOS/decimalClass.py
@@ -0,0 +1,43 @@
+"""Ejercicio:
+    2) Crear una clase llamada Decimal
+       con un unico atributo: valorNumerico
+       con los metodos convertirABinario, conventirAOctal, convertirAHexadecimal,  """
+
+
+class Decimal:
+    def __init__(self, valorNumerico):
+        self.valorNumerico = valorNumerico
+
+    def convertirABinario(self):
+        conversion = bin(self.valorNumerico)
+        return conversion[2:]
+        
+    def conventirAOctal(self):
+        conversion = oct(self.valorNumerico)
+        return conversion[2:]
+
+    def convertirAHexadecimal(self):
+        conversion = hex(self.valorNumerico)
+        return conversion[2:]
+
+if __name__ == "__main__":
+    decimal1 = Decimal(8)
+    decimal2 = Decimal(15)
+    print("Decimal 8")
+    print(decimal1.convertirABinario())
+    print(decimal1.conventirAOctal())
+    print(decimal1.convertirAHexadecimal())
+
+    print("Decimal 15")
+    print(decimal2.convertirABinario())
+    print(decimal2.conventirAOctal())
+    print(decimal2.convertirAHexadecimal())
+
+
+
+"""
+crear las clases Decimal,Binario Octal, Hexadecimal 
+e incluya los metodos necesarios para realizar las conversiones
+a los otros sistemas numericos
+"""
+
