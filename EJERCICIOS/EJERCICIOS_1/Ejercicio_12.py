2af3c0d7b2ead4983353de6770689e92717e3483
S4LGado <69695137+S4LGado@users.noreply.github.com>
2023-02-24 14:32:27 -0500

    as

--- EJERCICIOS/EJERCICIOS_1/Ejercicio_12.py
+++
@@ -1,9 +0,0 @@
-#==>  EJERCICIO 12 
-""" Un proyectil es lanzado verticalmente hacia arriba, calcule la velocidad inicial que debe tener para 
-alcanzar una altura dada. """
-
-h = float (input (" Digite la altura en la cual desea que llegue el proyectil en metros: "))
-
-vo = (2*h*9.82)**(1/2)
-
-print ("La velocidad con la cual debería salir despegado es de: ", vo, " m/s")
--- EJERCICIOS/EJERCICIOS_3/Ejercicio_4.py
+++
@@ -1,9 +0,0 @@
-""" Ejercicio empleados """
-empleados= ["Emplea_1" , "Emplea_2 ", " Emplea_3",  "Emplea_4,",  "Emplea_5",  "Emplea_6",  " Emplea_7",  "Emplea_8",  "Emplea_9",  "Emplea_10",  "Emplea_11",  "Emplea_12 " ,"Emplea_13 " ,"Emplea_14 ", "Emplea_15" , "Emplea_16",  "Emplea_17 ", "Emplea_18",  "Emplea_19",  "Emplea_20",  "Emplea_21" , "Emplea_22" , "Emplea_23 ", "Emplea_24",  "Emplea_25",  "Emplea_26",  "Emplea_27" ]
-rendimiento= ["C" ,      "B" ,     "B" ,       "B"   ,     "C" ,     "B" ,     "A" ,       "C"    ,   "B"  ,      "A"  ,      "C"   ,    "B"   ,     "B"    ,    "B"     ,    "B"    ,    "A"    ,   "B"      ,  "A"         ,"A"   ,     "C" ,      "B" ,       "B"   ,     "B"    ,     "B"  ,       "C" ,      "A" , "B"]
-
-cont=0
-tamano= len(empleados)
-for i in range(0,tamano):
-    if rendimiento[i] == "A":
-        print(empleados[i], rendimiento[i])
