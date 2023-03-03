332bd25a0f922aebdb151773416776e247761ba2
S4LGado <69695137+S4LGado@users.noreply.github.com>
2023-03-03 14:58:58 -0500

    a

---
+++ EJERCICIOS/04_Metodos.py
@@ -0,0 +1,24 @@
+#Metodos de las cadenas
+
+print(dir("hola"))
+
+print("hola mundo cruel".isalnum)
+
+lista = [1,2,3,4]
+
+print(lista.reverse)
+
+cadena = 'Hola mundo cruel'
+print("directorio", dir(cadena))
+print(cadena.upper())
+print(cadena.lower())
+print(cadena.center())
+cadena.strip() 
+#Metodos de las tuplas
+
+tupla = (10,20,30)
+print(dir(tupla))
+
+#Metodos de los diccionarios
+
+diccionario = {1 : 'uno', 2 : "dos" , 3:"tres"}
