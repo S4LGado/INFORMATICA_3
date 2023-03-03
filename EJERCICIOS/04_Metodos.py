212044fe57d9c2f250aca3cb11f9b32e9230ee4f
S4LGado <69695137+S4LGado@users.noreply.github.com>
2023-03-03 15:38:45 -0500

    a

--- EJERCICIOS/04_Metodos.py
+++ EJERCICIOS/04_Metodos.py
@@ -22,3 +22,51 @@ print(dir(tupla))
 #Metodos de los diccionarios
 
 diccionario = {1 : 'uno', 2 : "dos" , 3:"tres"}
+
+#==> EJERCICIO 1
+"A continuación se encuentra el poema el salmon"
+
+"""
+TÍTULO: EL SALMÓN
+PÁRRAFO 1: DETRÁS DE UN SALMÓN, NADA UN TIBURÓN, LO CAZA EN ALASKA, CANSADOS LOS DOS. 
+PÁRRAFO 2: ASUSTADO GRITA: ¡NOOO! POR FAVOR,MI VIDA ES MUY CORTA, ¡MUESTRA COMPASIÓN!.
+PÁRRAFO 3: ABRIENDO SU BOCA, LO DEJA ESCAPAR, Y CORRIENTE ARRIBA, LO HA VISTO NADAR.
+"""
+
+"""a) Corrija el poema de tal manera que:
+      Los indicadores de título y párrafo desaparezcan
+      Corregir el uso de mayúsculas y minúsculas según las reglas gramaticales.
+      El titulo esté en formato de título.
+      Separar los cuatro versos de cada párrafo en renglones sucesivos
+      Generar un espacio de renglon entre titulo-parrafo y párrafo-parrafo
+      El título debe estar centrado.   
+   b) Contar el número de veces que aparece cada vocal
+      contar el numero de lineas del poema.
+      Reemplazar las palabras: salmón ==> tiburón
+                               tiburón ==> salmón
+   c) Verificar si el contenido del poemas es: alfabetico
+                                               alfanumerico
+                                               decimal
+                                               digitos
+   d) Utilizar la indexación para extraer los elementos ubicados
+      en los índices: 0, 10, 100, último índices
+   e) Utilizar slicing para extraer los elementos ubicados en:
+      - Indices pares.
+      - Indices impares.
+      - Cada quinto elemento, empezando desde el 20
+      - Cada tercer elemento, pero empezando desde el indice 4 y terminando en el 62
+      - Desde la mitad hacia adelante
+      - Todos pero al revés
+      - Cada segundo elemento, pero al revés"""
+
+poema1 = """
+TÍTULO: EL SALMÓN
+PÁRRAFO 1: DETRÁS DE UN SALMÓN, NADA UN TIBURÓN, LO CAZA EN ALASKA, CANSADOS LOS DOS. 
+PÁRRAFO 2: ASUSTADO GRITA: ¡NOOO! POR FAVOR,MI VIDA ES MUY CORTA, ¡MUESTRA COMPASIÓN!.
+PÁRRAFO 3: ABRIENDO SU BOCA, LO DEJA ESCAPAR, Y CORRIENTE ARRIBA, LO HA VISTO NADAR.
+"""
+
+poema2 = poema1.replace('TÍTULO:',"")
+poema3 = poema2.replace("PÁRRAFO 1:")
+poema4 = poema3.replace("PÁRRAFO 1:")
+poema5 = poema4.replace("PÁRRAFO 1:")
