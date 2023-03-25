0fe37b9b0493d8188c5b890c50f82d0b13321234
S4LGado <juanessalgado2001@gmail.com>
2023-03-24 20:35:39 -0500

    a

---
+++ INFORMES/ejercicios1.py
@@ -0,0 +1,13 @@
+nombre_completo = "Juan Esteban Salgado" 
+
+# EJERCICIO 1
+
+def esMayorDeEdad(Edad):
+    MayorDeEdad = False
+    if Edad >= 18:
+        MayorDeEdad = True
+    return MayorDeEdad
+
+print(esMayorDeEdad(20))
+        
+
