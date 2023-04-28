939c9959dddf0bb2e5525045cafa70ccae088bd0
S4LGado <69695137+S4LGado@users.noreply.github.com>
2023-04-28 16:44:27 -0500

    as

--- EJERCICIOS/10_a_Simulacionelectrones.py
+++ EJERCICIOS/10_a_Simulacionelectrones.py
@@ -1,30 +1,81 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-k = 1
+k=1
 r = 1
-temp = 0.01
+T = 0.01
+q = 1
+elec_Circunferenciales = 50
+elec_Internos = 100
 
-elec_Circunferenciales = 15
-elec_Internos = 1000
+def crear_Estado_Inicial():
+    angulo = 6.28/(elec_Circunferenciales)
+    x_out = [r*np.cos(ang) for ang in np.arange(0, 6.28, angulo)]
+    y_out = [r*np.sin(ang) for ang in np.arange(0, 6.28, angulo)]
+    x_in = [np.random.random() - 0.5 for i in range(elec_Internos)]
+    y_in = [np.random.random() - 0.5 for i in range(elec_Internos)]
+    return x_out,y_out,x_in,y_in
 
-angulo = 6.28/elec_Circunferenciales
-x_out = [r*np.cos(ang) for ang in np.arange(0, 6.28, angulo)]
-y_out = [r*np.sin(ang) for ang in np.arange(0, 6.28, angulo)]
+def dibujar_sistema(x_out,y_out,x_in,y_in):
+    plt.figure()
+    plt.plot(x_out,y_out, "ro")
+    plt.plot(x_in, y_in, "bo")
+    plt.gca().set_aspect("equal")
+    plt.xlim(-1.1, 1.1)
+    plt.ylim(-1.1, 1.1)
+    plt.grid()
+    plt.show()
 
-x_in1 = [0.9*r*np.cos(ang-3) for ang in np.arange(0, 6.28, angulo)]
-y_in1 = [0.9*r*np.sin(ang-3) for ang in np.arange(0, 6.28, angulo)]
+x_out,y_out, x_in,y_in = crear_Estado_Inicial()
+dibujar_sistema(x_out,y_out,x_in,y_in)
 
-x_in = [0.95*r*np.cos(ang-2) for ang in np.arange(0, 6.28, angulo)]
-y_in = [0.95*r*np.sin(ang-2) for ang in np.arange(0, 6.28, angulo)]
+r=[]
+for i in range(len(x_out)):
+    r.append((x_out[i], y_out[i]))
+for i in range(len(x_in)):
+    r.append((x_in[i], y_in[i]))
 
+print( "======", r)
+def distancia(r1, r2):
+    return ((r1[0]-r2[0])**2 + (r1[1]-r2[1])**2)**0.5
 
-plt.figure()
-plt.plot(x_out,y_out,"ro")
-plt.plot(x_in, y_in, "bo")
-plt.plot(x_in1, y_in1, "yo")
-plt.plot()
-plt.gca().set_aspect("equal")
-plt.xlim(-1.1, 1.1)
-plt.ylim(-1.1, 1.1)
-plt.show
+from itertools import combinations
+def calcular_energia_total(r):
+    sumEnergias = 0
+    combinaciones = list(combinations(r, 2))
+    for r_ in combinaciones:
+        r1, r2 = r_[0], r_[1]
+        sumEnergias += k*q*q / distancia(r1,r2)
+    return sumEnergias
+
+sumEnergias_old = calcular_energia_total(r)
+print(sumEnergias_old)
+def metropolis(x_in,y_in,sumEnergias_old):  #cambio aleatorio de la posicion de un electron interno
+    x_old = x_in
+    y_old = y_in
+    x_new = [np.random.random() - 0.5 for i in range(elec_Internos)]
+    y_new = [np.random.random() - 0.5 for i in range(elec_Internos)]
+    r=[]
+    for i in range(len(x_out)):
+      r.append((x_out[i], y_out[i]))
+    for i in range(len(x_new)):
+      r.append((x_new[i], y_new[i]))
+    sumEnergias_new = calcular_energia_total(r)
+    if sumEnergias_new < sumEnergias_old:
+      sumEnergias_old = sumEnergias_new
+      print(sumEnergias_old)
+      x_in = x_new
+      y_in = y_new
+    else:
+      pass
+    return x_new,y_new,x_in,y_in,sumEnergias_old
+
+def paso_montecarlo(sumEnergias_old):
+    x_new,y_new,x_in,y_in,sumEnergias_old = [],[],[],[],sumEnergias_old
+    for i in range(0,100000):
+      x_new,y_new,x_in,y_in,sumEnergias_old = metropolis(x_in,y_in,sumEnergias_old)
+    return x_new,y_new,x_in,y_in,sumEnergias_old
+
+x_new,y_new,x_in,y_in,sumEnergias_old = paso_montecarlo(sumEnergias_old)
+
+dibujar_sistema(x_out,y_out,x_in,y_in)
